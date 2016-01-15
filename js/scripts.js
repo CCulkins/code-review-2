68435ffa10d6c58e8994c298fbd4bd3b89e7091a
cc <student@epicodus.com>
2016-01-15 11:37:10 -0800

    change form code

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,12 @@ $(document).ready(function() {
     var relaxationInput = $("select#relaxation").val();
     var cultureInput = $("select#culture").val();
 
-
-    if (VarInput === 'variablegoeshere' && VarInput=== 'variablegoeshere') {
-       var vacationMatch = "Spain";
-    if (VarInput === 'variablegoeshere' && VarInput=== 'variablegoeshere') {
-       var vacationMatch = "Spain";
-    if (VarInput === 'variablegoeshere' && VarInput=== 'variablegoeshere') {
-        var vacationMatch = "Spain";
+    if ("sum1" > "sum2" && "sum1" > "sum3" && "sum2" >= "sum3") {
+        var vacationMatch = "Sun";
+  //  if (VarInput === >7 <12) {
+  //     var vacationMatch = "Snow";
+  //  if (VarInput === >11 <16) {
+  //      var vacationMatch = "Mid";
 
 
     $("span#match").text(vacationmatch);
